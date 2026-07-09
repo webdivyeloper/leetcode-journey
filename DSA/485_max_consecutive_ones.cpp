5140692607fcc43c37949a1ce8c01b6cf3637b88
webdivyeloper <divyjain1969@gmail.com>
2026-07-09 10:20:55 +0530

    Create 485_max_consecutive_ones.cpp

---
+++ DSA/485_max_consecutive_ones.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+        int maxcount = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] == 1) {
+                count++;
+                maxcount = max(count,maxcount);
+            }
+            else count = 0;
+        }
+        return maxcount;
+    }
+};
